f9afb8b8d1b8b3ad89679037459bf708e7b15f77
sergeyvakhrin <parlamentsv@mail.ru>
2024-11-07 16:16:15 +0300

    add django forms method POST

---
+++ sitewomen/women/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+
+from women.models import Category, Husband
+
+
+class AddPostForm(forms.Form):
+    title = forms.CharField(max_length=255)
+    slug = forms.SlugField(max_length=255)
+    content = forms.CharField(widget=forms.Textarea(), required=False)
+    # widget=forms.Textarea() для построчного ввода, required=False не обязательное для заполнения
+    is_published = forms.BooleanField(required=False)
+    cat = forms.ModelChoiceField(queryset=Category.objects.all())
+    husband = forms.ModelChoiceField(queryset=Husband.objects.all(), required=False)
+
+
